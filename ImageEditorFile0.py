c322f5e5b34331ee427e42d8a77164a46323dd35
Tarsila <tarsila@alatazan.com>
2023-04-17 05:58:25 +0200

    First Commit

---
+++ ImageEditorFile0.py
@@ -0,0 +1,21 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut = './editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f'{filename}')
+
+    edit = img.filter(ImageFilter.SHARPEN)
+
+    clean_name = os.path.splitext(filename) [0]
+
+    edit.save(f'{clean_name}_edited.jpg')
+
+
+
+
+
+#Sources: https://www.youtube.com/watch?v=vEQ8CXFWLZU&list=PLsCTnBEpCwH-MZja6wejVZ6sdMbghuGYd&index=4&t=278s
+#Date: 17.04.23
